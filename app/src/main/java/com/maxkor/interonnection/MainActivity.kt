9100ed3e0a204fb5e3eda6d100a4da4dde31e1bf
M4XK8R <kpopmyhope@gmail.com>
2024-01-15 15:41:35 +0300

    Initial commit

---
+++ app/src/main/java/com/maxkor/interonnection/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.maxkor.interonnection
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.maxkor.interonnection.ui.theme.InterСonnectionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            InterСonnectionTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
+
